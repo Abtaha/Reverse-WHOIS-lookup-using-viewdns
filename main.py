70407b6c09d89ee66c485342b134310d3eff199b
Abtaha <abtaha2005@gmail.com>
2020-01-16 20:00:05 +0500

    Changed to bs4

--- main.py
+++ main.py
@@ -1,30 +1,21 @@
-from selenium.webdriver import Chrome
-from selenium.webdriver.chrome.options import Options
+# Import libraries
+from urllib.request import Request, urlopen
+import time
+from bs4 import BeautifulSoup as soup
 
 
-def getData(browser, input):
-    browser.get(f"https://viewdns.info/reversewhois/?q={input}")
-    
-    rows = browser.find_elements_by_tag_name("tr")
-    data = []
+try:
+    inp = input("Enter the name of the organization >> ")
+    req = Request(f'https://viewdns.info/reversewhois/?q={inp}', headers={'User-Agent': 'Mozilla/5.0'})
+    webpage = urlopen(req).read()
+
+    soup = soup(webpage, "html.parser")
+    rows = soup.findAll("table")[3].findAll("tr")#.findAll("rows")
+
+
     for row in rows:
-        data.append(row.find_elements_by_tag_name("td"))
-    
-    return data
-
-opts = Options()
-opts.headless = True
-opts.add_argument('log-level=2')
-assert opts.headless
-browser = Chrome(options=opts)
-
-inp = input("Enter the Registrant Name or Email Address >> ")
-data = getData(browser, inp)
-
-for i in data:
-    for x in i:
-        if x:
-            print(f"{x.text}", sep="    ")
-    print()
-
-browser.close()
+        for cell in row:
+            print(cell.text, sep="      ")
+        print()
+except:
+    print("Error")
